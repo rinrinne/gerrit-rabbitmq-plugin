15846595ff64dcbfcf63105f6579a4348592f386
rinrinne <rinrin.ne@gmail.com>
2014-01-09 12:08:24 +0900

    Restructured Keys

--- src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Keys.java
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Keys.java
@@ -1,30 +1,39 @@
 package com.googlesource.gerrit.plugins.rabbitmq;
 
 public enum Keys {
-  AMQP_URI("amqp.uri", null),
-  AMQP_USERNAME("amqp.username", null),
-  AMQP_PASSWORD("amqp.password", null),
-  AMQP_QUEUE("amqp.queue", null),
-  AMQP_EXCHANGE("amqp.exchange", null),
-  AMQP_ROUTINGKEY("amqp.routingKey", null),
-  MESSAGE_DELIVERY_MODE("message.deliveryMode", null),
-  MESSAGE_PRIORITY("message.priority", null),
-  GERRIT_NAME("gerrit.name", "gerrit-name"),
-  GERRIT_HOSTNAME("gerrit.hostname", "gerrit-host"),
-  GERRIT_SCHEME("gerrit.scheme", "gerrit-scheme"),
-  GERRIT_PORT("gerrit.port", "gerrit-port"),
-  GERRIT_FRONT_URL("gerrit.canonicalWebUrl", "gerrit-front-url"),
-  GERRIT_VERSION("gerrit.version", "gerrit-version"),
-  CONNECTION_MONITOR_INTERVAL("monitor.interval", null);
+  AMQP_URI("amqp.uri", null, "amqp://localhost"),
+  AMQP_USERNAME("amqp.username", null, "guest"),
+  AMQP_PASSWORD("amqp.password", null, "guest"),
+  QUEUE_NAME("queue.name", null, ""),
+  QUEUE_DURABLE("queue.durable", null, true),
+  QUEUE_AUTODELETE("queue.autoDelete", null, false),
+  QUEUE_EXCLUSIVE("queue.exclusive", null, false),
+  EXCHANGE_NAME("exchange.name", null, ""),
+  EXCHANGE_TYPE("exchange.type", null, "fanout"),
+  EXCHANGE_DURABLE("exchange.durable", null, false),
+  EXCHANGE_AUTODELETE("exchange.autoDelete", null, false),
+  BIND_ROUTINGKEY("bind.routingKey", null, ""),
+  MESSAGE_DELIVERY_MODE("message.deliveryMode", null, 1),
+  MESSAGE_PRIORITY("message.priority", null, 0),
+  MESSAGE_ROUTINGKEY("message.routingKey", null, ""),
+  GERRIT_NAME("gerrit.name", "gerrit-name", ""),
+  GERRIT_HOSTNAME("gerrit.hostname", "gerrit-host", ""),
+  GERRIT_SCHEME("gerrit.scheme", "gerrit-scheme", "ssh"),
+  GERRIT_PORT("gerrit.port", "gerrit-port", 29418),
+  GERRIT_FRONT_URL("gerrit.canonicalWebUrl", "gerrit-front-url", ""),
+  GERRIT_VERSION("gerrit.version", "gerrit-version", null),
+  MONITOR_INTERVAL("monitor.interval", null, 15000);
 
   public String section;
-  public String value;
-  public String header;
+  public String name;
+  public String key;
+  public Object defaultVal;
 
-  Keys(String property, String header) {
+  Keys(String property, String key, Object defaultVal) {
     String[] part = property.split("\\.");
     this.section = part[0];
-    this.value = part[1];
-    this.header = header;
+    this.name = part[1];
+    this.key = key;
+    this.defaultVal = defaultVal;
   }
 }
